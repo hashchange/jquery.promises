fd2591b36504553f0c263a145764e840bf591ac3
hashchange <heim@zeilenwechsel.de>
2014-07-25 11:19:21 +0200

    Added demo validating the AMD build

--- demo/demo-amd/amd.js
+++ demo/demo-amd/amd.js
@@ -15,5 +15,15 @@ require( [
 
 ], function ( $ ) {
 
+    // Check that $.Promises is available
+    var $message = $( '<div class="alert-box large-text-center">' );
+
+    if ( !!$.Promises ) {
+        $message.addClass( "success" ).text( "$.Promises is available, as expected." );
+    } else {
+        $message.addClass( "alert" ).text( "Something went wrong: $.Promises is not available!" );
+    }
+
+    $( "#content" ).append( $message );
 
 } );
