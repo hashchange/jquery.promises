fe278d78bda6b657a8e9b5dc980c82c51a35801f
hashchange <heim@zeilenwechsel.de>
2015-08-22 16:27:07 +0200

    Changed demo directory structure, clarified purpose

--- demo/amd/amd.js
+++ demo/amd/amd.js
@@ -1,12 +1,4 @@
-requirejs.config( {
-
-    baseUrl: '../../bower_components',
-
-    paths: {
-        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
-        'jquery.promises': '/dist/amd/jquery.promises'
-    }
-} );
+// amd.js
 
 require( [
 
---
+++ demo/amd/require-config.js
@@ -0,0 +1,9 @@
+requirejs.config( {
+
+    baseUrl: '../../bower_components',
+
+    paths: {
+        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
+        'jquery.promises': '/dist/amd/jquery.promises'
+    }
+} );
